bb4872c7ef1bcb7fe15b04f3e2a37a0a753f66bb
wonderpops <supstump@gmail.com>
2021-09-06 17:09:39 +1000

    Auth checker restore

--- lib/widgets/layouts/main_layout.dart
+++ lib/widgets/layouts/main_layout.dart
@@ -1,4 +1,7 @@
 import 'package:crabs_trade/constants/style.dart';
+import 'package:crabs_trade/helpers/session_model.dart';
+import 'package:crabs_trade/widgets/auth/auth_model.dart';
+import 'package:crabs_trade/widgets/auth/auth_widget.dart';
 import 'package:crabs_trade/widgets/layouts/small_screen_layout.dart';
 import 'package:crabs_trade/widgets/nav_bar/top_nav.dart';
 import 'package:crabs_trade/widgets/side_menu/side_menu_widget.dart';
@@ -7,6 +10,7 @@ import '../../helpers/responsiveness.dart';
 import 'large_screen_layout.dart';
 
 class MainLayout extends StatelessWidget {
+  final sessionModel = SessionModel();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   final String route;
 
@@ -14,23 +18,59 @@ class MainLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      backgroundColor: dark,
-      appBar: topNavBar(context, _scaffoldKey),
-      drawer: Drawer(
-        child: SideMenuWidget(
-          route: route,
-        ),
-      ),
-      body: ResponsiveWidget(
-          largeScreen: LargeScreenLayout(
-            route: route,
+    return FutureBuilder(
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If we got an error
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                '${snapshot.error} occurred',
+                style: const TextStyle(fontSize: 18),
+              ),
+            );
+
+            // if we got our data
+          } else if (snapshot.hasData) {
+            // Extracting data from snapshot object
+            final isAuth = snapshot.data as bool;
+            if (isAuth == true) {
+              return Scaffold(
+                key: _scaffoldKey,
+                backgroundColor: dark,
+                appBar: topNavBar(context, _scaffoldKey),
+                drawer: Drawer(
+                  child: SideMenuWidget(
+                    route: route,
+                  ),
+                ),
+                body: ResponsiveWidget(
+                    largeScreen: LargeScreenLayout(
+                      route: route,
+                    ),
+                    mediumScreen: LargeScreenLayout(
+                      route: route,
+                    ),
+                    smallScreen: SmallScreenLayout()),
+              );
+            } else {
+              return AuthProvider(
+                  model: AuthModel(), child: const AuthWidget());
+            }
+          }
+        }
+        // Displaying LoadingSpinner to indicate waiting state
+        return Container(
+          color: dark,
+          child: const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: light,
+              color: active,
+            ),
           ),
-          mediumScreen: LargeScreenLayout(
-            route: route,
-          ),
-          smallScreen: const SmallScreenLayout()),
+        );
+      },
+      future: sessionModel.checkAuth(),
     );
   }
 }
